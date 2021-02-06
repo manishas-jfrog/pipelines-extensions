f79ea5ef73b1cff21e3769586431deff48064df0
Manisha Sahasrabudhe <manishas@jfrog.com>
2021-02-06 15:40:46 -0800

    gitrepo input

--- steps/ansible/ansiblePlaybook/onExecute.sh
+++ steps/ansible/ansiblePlaybook/onExecute.sh
@@ -1,12 +1,12 @@
 execute_step() {
-  local resourceName=$(find_step_configuration_value resourceName)
-  local fileName=$(find_step_configuration_value fileName)
-  local gitRepoName=$(find_resource_variable $resourceName gitRepoName)
-  local resourcePath=$(find_resource_variable $resourceName resourcePath)/$gitRepoName
-  echo "Changing directory: $resourcePath"
-  pushd $resourcePath
+  # local resourceName=$(find_step_configuration_value resourceName)
+  # local fileName=$(find_step_configuration_value fileName)
+  # local gitRepoName=$(find_resource_variable $resourceName gitRepoName)
+  # local resourcePath=$(find_resource_variable $resourceName resourcePath)/$gitRepoName
+  # echo "Changing directory: $resourcePath"
+  # pushd $resourcePath
   ansible-playbook $fileName
-  popd
+  # popd
   echo "Executed ansible playbook defined in: $fileName"
 }
 execute_command execute_step
