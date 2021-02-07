40223ca3569a9a2b746ec10e3f9b23dd0aab09c1
Manisha Sahasrabudhe <manishas@jfrog.com>
2021-02-06 16:30:13 -0800

    correcting step

--- steps/ansible/ansiblePlaybook/onExecute.sh
+++ steps/ansible/ansiblePlaybook/onExecute.sh
@@ -1,12 +1,10 @@
 execute_step() {
-  # local resourceName=$(find_step_configuration_value resourceName)
-  # local fileName=$(find_step_configuration_value fileName)
-  # local gitRepoName=$(find_resource_variable $resourceName gitRepoName)
-  # local resourcePath=$(find_resource_variable $resourceName resourcePath)/$gitRepoName
-  # echo "Changing directory: $resourcePath"
-  # pushd $resourcePath
-  buildDir=$(find_resource_variable "$inputGitRepoResourceName" resourcePath)
-  pushd $buildDir
+  local resourceName=$(find_step_configuration_value resourceName)
+  local fileName=$(find_step_configuration_value fileName)
+  local gitRepoName=$(find_resource_variable $resourceName gitRepoName)
+  local resourcePath=$(find_resource_variable $resourceName resourcePath)/$gitRepoName
+  echo "Changing directory: $resourcePath"
+  pushd $resourcePath
   ansible-playbook $fileName
   popd
   echo "Executed ansible playbook defined in: $fileName"
